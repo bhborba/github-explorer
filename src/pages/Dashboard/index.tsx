19ceec6f5b3bfb6db2e28698a29e238fce0ed69f
Bruno Henrique de Borba <brunohenriqueborba@gmail.com>
2020-12-21 14:46:21 -0300

    Dashboard styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+          <img
+            src="https://avatars0.githubusercontent.com/u/38145205?s=460&u=ddb86c4c108cefc8d5c8b96962ab77754d7860c9&v=4"
+            alt="Bruno"
+          />
+
+          <div>
+            <strong>teste/teste</strong>
+            <p>Descrição aee</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
